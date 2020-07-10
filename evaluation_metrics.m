747c432cc8ba82ba458326a25c886fded58f43cc
Christy Liong <stliong@o365.fcu.edu.tw>
2020-07-10 12:24:18 +0800

    Add files via upload

---
+++ evaluation_metrics.m
@@ -0,0 +1,4 @@
+A = confusionmat(predictedLabels, desiredLabels); 
+Acc = (A(1)+A(5)+A(9) )/ sum(A(:));
+UAR = (A(1)/sum(A(1:3)) + A(5)/sum(A(4:6)) + A(9)/sum(A(7:9)))/3;
+UF1 =( (2A(1)/ (2A(1)+A(2)+A(3)+A(4)+A(7)))+ (2A(5)/ (2A(5)+A(2)+A(8)+A(4)+A(6)))+ (2A(9)/ (2A(9)+A(3)+A(6)+A(7)+A(8))))/3;
